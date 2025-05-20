6378592d4e9a8dca30d4b8db6dbfc7755408bc82
Minh Nguyen <pearl.minh43@gmail.com>
2025-05-20 12:55:13 +0700

    update

--- components/component-tabs.tsx
+++ components/component-tabs.tsx
@@ -38,34 +38,33 @@ export function ComponentTabs({ name, children, className }: ComponentTabsProps)
   }, [name])
 
   return (
-    <div
-      className={cn(
-        "size-full overflow-hidden rounded-lg border bg-background not-prose max-h-[438px]",
-        className
-      )}
-    >
-      <Tabs defaultValue="preview" className="size-full gap-0">
-        <TabsList className="w-full rounded-none border-b">
+    <div className={cn("rounded-lg border bg-background", className)}>
+      <Tabs defaultValue="preview" className="gap-0 not-prose">
+        <TabsList className="w-full rounded-b-none border-b text-muted-foreground">
           <TabsTrigger value="preview">
-            <EyeIcon size={16} className="text-muted-foreground" />
+            <EyeIcon size={16} />
             Preview
           </TabsTrigger>
+
           <TabsTrigger value="code">
-            <CodeIcon size={16} className="text-muted-foreground" />
+            <CodeIcon size={16} />
             Code
           </TabsTrigger>
         </TabsList>
-        <TabsContent value="code" className="component-block">
-          {Code}
+
+        <TabsContent
+          value="preview"
+          className="relative flex items-center justify-center p-6 min-h-[438px]"
+        >
+          <div className="-translate-y-px absolute top-4 right-0 left-0 border border-border/50 border-dashed" />
+          <div className="absolute right-0 bottom-4 left-0 translate-y-px border border-border/50 border-dashed" />
+          <div className="-translate-x-px absolute top-0 bottom-0 left-4 border border-border/50 border-dashed" />
+          <div className="absolute top-0 right-4 bottom-0 translate-x-px border border-border/50 border-dashed" />
+          {Preview}
         </TabsContent>
-        <TabsContent value="preview" className="not-fumadocs-codeblock size-full overflow-hidden">
-          <div className="relative flex size-full flex-col items-center justify-center gap-4 overflow-hidden p-10">
-            <div className="-translate-y-px absolute top-6 right-0 left-0 border border-border/50 border-dashed" />
-            <div className="absolute right-0 bottom-6 left-0 translate-y-px border border-border/50 border-dashed" />
-            <div className="-translate-x-px absolute top-0 bottom-0 left-6 border border-border/50 border-dashed" />
-            <div className="absolute top-0 right-6 bottom-0 translate-x-px border border-border/50 border-dashed" />
-            {Preview}
-          </div>
+
+        <TabsContent value="code" className="component-block max-h-[438px]">
+          {Code}
         </TabsContent>
       </Tabs>
     </div>
