bd3603e6b249f3264a5d70e49ccc3fb7b2457e9b
Minh Nguyen <pearl.minh43@gmail.com>
2025-06-02 15:12:49 +0700

    update

--- components/component-introduce.tsx
+++ components/component-introduce.tsx
@@ -4,7 +4,6 @@ import { Cover } from "@/components/ui/cover"
 import { typography } from "@/lib/typography"
 import { cn } from "@/lib/utils"
 import { motion } from "motion/react"
-import React from "react"
 import { MorphingText } from "./ui/morphing-text"
 
 export function ComponentIntroduce() {
@@ -12,11 +11,22 @@ export function ComponentIntroduce() {
     <div>
       <div className="relative max-w-7xl mx-auto text-center">
         <Cover>
-          <span className="text-2xl md:text-3xl lg:text-4xl">₊˚✧ </span>
-          <span className={`${typography.EmilysCandy} text-5xl md:text-6xl lg:text-7xl`}>
-            Laviecn
-          </span>
-          <span className="text-2xl md:text-3xl lg:text-4xl"> ✧˚₊</span>
+          <motion.div
+            className="bg-[linear-gradient(110deg,#404040,35%,#fff,50%,#404040,75%,#404040)] bg-[length:200%_100%] bg-clip-text text-transparent"
+            initial={{ backgroundPosition: "200% 0" }}
+            animate={{ backgroundPosition: "-200% 0" }}
+            transition={{
+              repeat: Infinity,
+              duration: 5,
+              ease: "linear",
+            }}
+          >
+            <span className="text-2xl md:text-3xl lg:text-4xl">₊˚✧ </span>
+            <span className={`${typography.EmilysCandy} text-5xl md:text-6xl lg:text-7xl`}>
+              Laviecn
+            </span>
+            <span className="text-2xl md:text-3xl lg:text-4xl"> ✧˚₊</span>
+          </motion.div>
         </Cover>
       </div>
       <motion.div
