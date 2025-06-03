f4e897ded7f6bb90f70c8cf00b30653cc02831d5
Nguyen Ngoc Minh <pearl.minh43@gmail.com>
2025-06-03 11:59:08 +0700

    update

--- components/components-list.tsx
+++ components/components-list.tsx
@@ -4,20 +4,40 @@ import { source } from "@/lib/source"
 
 export function ComponentsList() {
   const components = source.pageTree
-
   const list = components.children.filter((component) => component.type === "page")
 
+  const groups = [
+    {
+      title: "Components",
+      items: list.filter((item) => item.$id && item.$id.startsWith("components/")),
+    },
+    {
+      title: "Utils",
+      items: list.filter((item) => item.$id && item.$id.startsWith("utils/")),
+    },
+  ]
+
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-x-8 lg:gap-x-16 lg:gap-y-6 xl:gap-x-20">
-      {list.map((component) => (
-        <Link
-          key={component.$id}
-          href={component.url}
-          className="not-prose text-lg font-medium underline-offset-4 hover:underline md:text-base"
-        >
-          {component.name}
-        </Link>
-      ))}
+    <div className="mt-12 space-y-8">
+      {groups.map(
+        (group) =>
+          group.items.length > 0 && (
+            <div key={group.title}>
+              <div className="mb-4 text-xl font-medium">{group.title}</div>
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-x-8 lg:gap-x-16 lg:gap-y-6 xl:gap-x-20">
+                {group.items.map((component) => (
+                  <Link
+                    key={component.$id}
+                    href={component.url}
+                    className="not-prose text-lg text-muted-foreground font-medium underline-offset-4 hover:underline md:text-base"
+                  >
+                    {component.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          )
+      )}
     </div>
   )
 }
