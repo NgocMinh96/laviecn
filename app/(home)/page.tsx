69c5a3d5b8382d1cd16d8ea9a688fc3db72a7481
Minh Nguyen <pearl.minh43@gmail.com>
2025-06-05 12:17:02 +0700

    update

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -22,7 +22,7 @@ export default function HomePage() {
             as="button"
             className={cn(
               "bg-shine rounded-xl shadow bg-[length:200%_100%] tracking-wide",
-              "dark:bg-[linear-gradient(110deg,#09090B,45%,#27272A,55%,#09090B)] dark:text-zinc-200 dark:border-zinc-800",
+              "dark:bg-[linear-gradient(110deg,#09090B,45%,#3b3b32,55%,#09090B)] dark:text-zinc-200 dark:border-zinc-800",
               "bg-[linear-gradient(110deg,#FFF,45%,#E4E4E7,55%,#FFF)] text-zinc-800 border-zinc-300"
             )}
           >
